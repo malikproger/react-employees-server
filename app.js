187d6f733864c32cd1b3e8c5c192a2a50cdb70a5
Malik <malik28067@mail.ru>
2024-07-22 13:12:15 +0300

    feat: cors added

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const express = require('express')
-const path = require('path')
-const cookieParser = require('cookie-parser')
-const logger = require('morgan')
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-require('dotenv').config()
+require('dotenv').config();
 
-const app = express()
+const app = express();
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
+app.use(logger('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.use('/api/user', require('./routes/users'))
-app.use('/api/employees', require('./routes/employees'))
+app.use('/api/user', require('./routes/users'));
+app.use('/api/employees', require('./routes/employees'));
 
-module.exports = app
+module.exports = app;
